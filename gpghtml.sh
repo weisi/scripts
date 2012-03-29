b7fbe53b0da289c2cb4701905331191ac5164e39
Weisi Dai <multiple1902@gmail.com>
2012-03-29 16:44:49 +0800

    added gpghtml

---
+++ gpghtml.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Written by multiple1902 <multiple1902@gmail.com>
+# Released under MIT License
+#
+# gpghtml.sh:
+#   Sign a HTML page in cleartext using gnupg
+#
+# Usage:
+#   gpghtml.sh happyhour.html
+#
+# Requirements:
+#   gnupg
+#
+
+
+if [ -z "$1" ]; then
+    echo "gpghtml: please specify input file";
+    exit 126
+fi
+
+TMPFILE=$(mktemp)
+DEST=$(dirname "$1")/$(basename "$1" .html).signed.html
+
+cat > "$TMPFILE" << EOF
+
+-->
+EOF
+
+cat "$1" >> "$TMPFILE"
+
+cat >> "$TMPFILE" << EOF
+
+<!--
+EOF
+
+if gpg --clearsign "$TMPFILE"; then
+
+    cat > "$DEST" << EOF
+<!--
+EOF
+
+    cat >> "$DEST" < "$TMPFILE.asc"
+
+    cat >> "$DEST" << EOF
+-->
+EOF
+
+    rm "$TMPFILE" "$TMPFILE.asc"
+
+    echo "gpghtml: succ"
+
+else
+
+    echo "gpghtml: gpg failed to sign the file"
+    return 125
+
+fi
